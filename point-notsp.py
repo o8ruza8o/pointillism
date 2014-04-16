b70eb721a30c96d69f9896f0949dedea77742765
Ruza Markov <ruza.markov@gmail.com>
2014-04-15 22:49:13 -0700

    adding back a version without tsp and cleanup

--- point-notsp.py
+++ point-notsp.py
@@ -25,7 +25,7 @@ def projectImage(img, vector = np.ones(3, dtype = np.float32), power = 1.0):
 def drawCircle(ctx, center, radius, color):
     "Draw a circle using Cairo vector graphics."
     ctx.move_to(center[0] - radius, center[1])
-    ctx.arc(center[0], center[1], radius, -pi, pi)
+    ctx.arc(center[1], center[0], radius, -pi, pi)
     ctx.set_source_rgba(color[0], color[1], color[2], 0.9)
     ctx.fill()
     ctx.stroke()
@@ -91,55 +91,9 @@ def optimize(generators, centroids, n, shape, radius):
     return centroids
 
 
-def distance(l):
-    return sum([L2[l[i-1], l[i]] for i in range(len(l))])
-
-def twoOptSwap(route, i, k):
-    delta = (L2[route[i-1], route[k-1]] + L2[route[i], route[k]]
-            -L2[route[i-1], route[i]] -L2[route[k-1], route[k]])
-    
-    if delta > 0.1:
-        return route
-    else:
-        new_route = route[:]
-        new_route[i:k] = route[i:k][::-1]
-        return new_route
-
-def tsp():
-    # calculate metric matrix
-    nPoints = len(points)
-    global L2
-    L2 = np.zeros([nPoints, nPoints], dtype = np.float32)
-    for i in range(nPoints):
-        for j in range(i):
-            L2[i, j] = L2[j, i] = np.sqrt(sum((points[i].p - points[j].p)**2))
-    print "metric created!"
-
-    # 2opt
-    best_route = range(nPoints)
-    best_distance = distance(best_route)
-    route = range(nPoints)
-    for count in range(int(np.sqrt(nPoints))):
-        for iInterp in range(nPoints**2 / 2):
-            i = iInterp // nPoints
-            k = iInterp %  nPoints
-            if k != i:
-                i, k = sorted([i, k])
-                route = twoOptSwap(route, i, k)
-
-        route_distance = distance(route)
-        if best_distance > route_distance+1:
-             best_route, best_distance = route[:], route_distance
-             print route_distance
-        else:
-            break
-
-    return [points[i] for i in best_route]
-
-
 if __name__=='__main__':
     # get the image to be stippled
-    filename = "sierpinski.png"
+    filename = "haeckel.jpeg"
     img = misc.imread(filename)
     global points
     points = []
@@ -153,18 +107,18 @@ if __name__=='__main__':
         carray, frequency = colorCluster(img, K)
     else:  # black stipples only
         K = 1
-        carray = np.array([[0.0] * 3])
+        carray = np.array([[255.0] * 3])
         frequency = [mean(img)/255.0 * img.shape[0] * img.shape[1]]
         
     for i in range(K):
         # project the image along a color vector
         col = carray[i]
-        gray_img = projectImage(img.astype(np.float32), col, 0.5).squeeze()
+        gray_img = projectImage(img.astype(np.float32), col, 0.2).squeeze()
 
         # get the dimensions of image and number of points to replace it with
         imshape = gray_img.shape
         # nPoints = int(float(frequency[i]) / 50.0)
-        nPoints = 10000
+        nPoints = 100000
 
         # initialize generators and centroids arrays
         generators =  np.zeros([2, nPoints], dtype = np.int)
@@ -183,36 +137,28 @@ if __name__=='__main__':
             points.append(Point(cen, col))
 
     # randomise order of points to plot
-    # random.shuffle(points)
-
-    # sort the list in case that is faster
-    sorted(points, key=lambda x: -sum(x.p**2))
-
-    # tsp through these points.
-    tsp_pts = []
-    tsp_pts = tsp()
+    random.shuffle(points)
 
     # Make a pdf surface
     surf =  cairo.PDFSurface(open(filename.split(".")[0]+str(K)+'point.pdf',
                                   "w"), img.shape[1], img.shape[0])
     # Make a svg surface
-    # surf =  cairo.SVGSurface(open("test.svg", "w"), img.shape[0], img.shape[1])
+    # surf =  cairo.SVGSurface(open("test.svg", "w"), img.shape[1], img.shape[0])
     
-    # Get a context object and set line width
+    # Get a context object from Cairo
     ctx = cairo.Context(surf)
-    ctx.set_line_width(0.5)
-
+    # Black background
+    ctx.rectangle(0.0, 0.0, img.shape[1], img.shape[2]);
+    ctx.set_source_rgb(0.0, 0.0, 0.5);
+    ctx.fill();
+    # Add circles
     xCoords = []
     yCoords = []
-    ctx.move_to(tsp_pts[-1].p[1], tsp_pts[-1].p[0])
     # Make a data file
     with  open(filename.split(".")[0]+'-data.csv', "w") as datafile:
         datafile.write("d,x,y,r,g,b\n")
-        for point in tsp_pts:
-            # drawCircle(ctx, point.p, radius, point.c/255.0)
-            ctx.set_source_rgb(point.c[0], point.c[1], point.c[2])
-            ctx.line_to(point.p[1], point.p[0])
-
+        for point in points:
+            drawCircle(ctx, point.p, radius/2.0, point.c/255.0)
             datafile.write(str(radius) + "," + 
                            str(point.p[0]) + "," + 
                            str(point.p[1]) + "," +
@@ -221,18 +167,6 @@ if __name__=='__main__':
                            str(int(point.c[2])) + "\n")
             xCoords.append(-point.p[0])
             yCoords.append(-point.p[1])
-    ctx.close_path()
-    ctx.fill()
     ctx.stroke()
     surf.finish()
-
-    # plot the data
-    pl.figure(1)
-    pl.plot(yCoords, xCoords, 'g-')
-    frame = pl.gca()
-    frame.axes.get_xaxis().set_visible(False)
-    frame.axes.get_yaxis().set_visible(False)
-    pl.savefig('tsp-' + filename, bbox_inches='tight')
-    pl.show()
-
     print len(points), " points polotted!"
